280e5f0ef23fddff1c619fc4d8d118ad5b5135a6
Amir Alekaev <dstation.ru@yandex.ru>
2023-03-31 21:09:56 +0300

    исправления в popap

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,14 @@ const popupElement = document.querySelector('.popup');
 const popupElementClose = popupElement.querySelector('.popup__button-close');       // находим кнопку для закрытия попапа редактирования профиля
 const popupElementOpen = document.querySelector('.profile__edit-button');           // находим кнопку для открытия попапа редактирования профиля
 const popupElementForm = popupElement.querySelector('.popup__form');                // Найти форму popup изменения профиля
-const popupCloseList = document.querySelectorAll('.popup__button-close');           //кнопка закрытия попап
+// const popupCloseList = document.querySelectorAll('.popup__button-close');           //кнопка закрытия попап
 
 let profileName = document.querySelector('.profile-info__name');                   // Найти данные - name на странице
 let profileJob = document.querySelector('.profile-info__job');
 //console.log(profileName);
 // Найти данные - job на странице
 let inputName = popupElement.querySelector('.popup__imput_type_name');                  // Найти поле ввода - name в форме редактирования профиля
-let inputJob = popupElement.querySelector('.popup__imput_type_job');                    // Найти поле ввода - job в форме редактирования профиля
+let inputJob = popupElement.querySelector('.popup__input_type_job');                    // Найти поле ввода - job в форме редактирования профиля
 
 function openPopup() {                                                     // открытие попап
     popupElement.classList.add('popup_opened');
@@ -31,5 +31,6 @@ function handleFormSubmit(evt) {
 
 popupElementOpen.addEventListener('click', openPopup);
 popupElementClose.addEventListener('click', closePopup);
-popupElement.addEventListener('click', closePopupClickOverlay);
+//popupElement.addEventListener('click', closePopupClickOverlay);
 popupElementForm.addEventListener('submit', handleFormSubmit);
+
